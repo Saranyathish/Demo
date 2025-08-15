c731931413c7ddf05ef08cbbc87c28401f52c053
Saranyathish <saranyanr@d-sat.com>
2025-08-15 21:46:54 +0530

    check

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 CI/CD POC Deployment Successful! 🚀</h1>
       <p>Version 1.0 - Deployed automatically from GitHub Actions to Netlify</p>
+      <h3>Done by saranya NR</h3>
     </div>
   );
 }
